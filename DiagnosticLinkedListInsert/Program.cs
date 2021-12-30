aff73180470166aa71ad97ce228d5919de55a8b8
Евгений Гилев <euggil@yandex.ru>
2021-12-30 13:38:55 +0500

    Добавьте файлы проекта.

---
+++ DiagnosticLinkedListInsert/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace DiagnosticLinkedListInsert
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // читаем весь файл с рабочего стола в строку текста
+            string text = File.ReadAllText(@"C:\temp1\Text1.txt");
+
+            // Сохраняем символы-разделители в массив
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+
+            // разбиваем нашу строку текста, используя ранее перечисленные символы-разделители
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            LinkedList<string> wordslist = new LinkedList<string>();
+
+            // Запустим таймер
+            var watch = Stopwatch.StartNew();
+
+            foreach (var word in words)
+            {
+                wordslist.AddLast(word);
+            }
+
+            Console.WriteLine($"Заполнили список за: {watch.Elapsed.TotalMilliseconds}  мс");
+
+            // Выполним вставку первым элементом
+            watch = Stopwatch.StartNew();
+            wordslist.AddFirst("картошка");
+
+            // Выведем результат
+            Console.WriteLine($"Вставка в  словарь первым элементом списка за : {watch.Elapsed.TotalMilliseconds}  мс");
+        }
+    }
+}
---
+++ DiagnosticListInsert/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace DiagnosticListInsert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // читаем весь файл с рабочего стола в строку текста
+            string text = File.ReadAllText(@"C:\temp1\Text1.txt");
+
+            // Сохраняем символы-разделители в массив
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+
+            // разбиваем нашу строку текста, используя ранее перечисленные символы-разделители
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> wordslist = new List<string>();
+
+            // Запустим таймер
+            var watch = Stopwatch.StartNew();
+
+            //заполнили список AddRange
+            wordslist.AddRange(words);
+            Console.WriteLine($"Заполнили список AddRange за: {watch.Elapsed.TotalMilliseconds}  мс");
+
+            //очистим список
+            wordslist.Clear();
+
+            watch = Stopwatch.StartNew();
+
+            //заполнили список в цикле foreach
+            foreach (var word in words)
+            {
+                wordslist.Add(word);
+            }
+
+            Console.WriteLine($"Заполнили список в цикле за: {watch.Elapsed.TotalMilliseconds}  мс");
+
+            watch = Stopwatch.StartNew();
+
+            // Выполним вставку
+            wordslist.Add("картошка");
+
+            // Выведем результат
+            Console.WriteLine($"Вставка в  словарь за : {watch.Elapsed.TotalMilliseconds}  мс");
+        }
+    }
+}
